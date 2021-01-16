33724d3c8bd2f911e84dc80f33b4dd1d41babeb5
RoZaMaFu <68646221+RoZaMaFu@users.noreply.github.com>
2021-01-16 10:36:28 +0300

    fix: wrote functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min (args) {
+  if(args.length != 0 && Math.min(...args)){
+        return Math.min(...args)
+    }
+    else {
+        return 0
+    }
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max (args) {
+  if(args.length != 0 && Math.max(...args)){
+        return Math.max(...args)
+    }
+    else {
+        return 0
+    }
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg (args) {
+  if(args.length != 0 && args.reduce((accum, number) => accum + number,0) / args.length){
+        return args.reduce((accum, number) => accum + number,0) / args.length
+    }
+    else {
+        return 0
+    }
 }
